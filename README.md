1669347e94e03f835e6a84f118a24cab0c737849
Pal Rachhadiya <palrachhadiya24@gmail.com>
2025-12-26 12:05:11 +0530

    initial commit: password salting demo in C

---
+++ README.md
@@ -0,0 +1,15 @@
+# Password Salting Demo (C)
+    This project demonstrates how password salting works using simple salting technique in C.
+# Disclaimer
+    I made this project only for educational purposes.
+    It does NOT use cryptographically secure hashing and must NOT be used in real applications.
+# Features 
+    * User Registration
+    * Password Salting
+    * Hash based login verification
+    * File based storage
+# How to Run
+    -> Compile the program using a C compiler (eg. gcc).
+    -> Run the executable from the terminal.
+# Learning Outcome
+    This project helped me understand file handling, structured program design and applying C programming concepts to a real-life problem.
---
+++ main.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+unsigned int simpleHash(char *str){
+    unsigned int hash =0;
+        for(int i=0; str[i]!= '\0'; i++){
+            hash = hash * 31 + str[i];
+        }
+        return hash;
+    }
+
+void generateSalt(char *salt){
+    char set[]= "abcdefghijklmnopqrstuvwxyz0123456789";
+    for(int i=0; i <6; i++){
+        salt[i] = set[rand() % 36];
+    }
+    salt[6] = '\0';
+}
+
+void UserRegister(){
+    char username[50];
+    char password[20];
+    char salt[7];
+    unsigned int hash;
+
+    printf("Set Username: ");
+    scanf("%49s", username);
+
+    printf("Set password: ");
+    scanf("%19s", password);
+    
+    generateSalt(salt);
+
+    char combine[70];
+    strcpy(combine, password);
+    strcat(combine, salt);
+
+    hash = simpleHash(combine);
+
+    FILE *fptr = fopen("users.txt", "a");
+    if( fptr == NULL){
+        printf("error opening file \n");
+        return;
+    }
+    fprintf(fptr, "%s %s %u\n", username, salt, hash);
+    fclose(fptr);
+
+    printf("USER REGISTERED SUCCESSFULLY! \n");
+}
+
+
+void UserLogin(){
+    char username[50];
+    char password[20];
+    char salt[7];
+    char fileuser[50];
+    unsigned int inputhash, storedhash;
+
+    printf("Enter Username: ");
+    scanf("%49s", username);
+
+    printf("Enter password: ");
+    scanf("%19s", password);
+
+    FILE *fptr = fopen ("users.txt", "r");
+    if( fptr == NULL){
+        printf("error opening file \n");
+        return;
+    }
+    while(fscanf (fptr, "%s %s %u", fileuser, salt, &storedhash) == 3){
+        if(strcmp (username,fileuser) == 0){
+            char combine[60];
+            strcpy(combine, password);
+            strcat(combine, salt);
+
+            inputhash = simpleHash(combine);
+
+            if (inputhash == storedhash){
+                printf("LOGIN SUCCESSFUL:) \n");
+            }else{
+                printf("WRONG PASSWORD :( \n");
+
+            }
+            fclose(fptr);
+            return;
+        }
+
+    }
+    fclose(fptr);
+    printf("USER NOT FOUND! PLEASE REGISTER\n");
+}
+
+int main(){
+    srand(time(NULL));
+    int choose;
+
+    while(1){
+        printf("\n 1) Register \n 2) Login \n 3)Exit \n CHOOSE: ");
+        scanf("%d", &choose);
+
+        if(choose == 1){
+            UserRegister();
+        }else if(choose == 2){
+            UserLogin();
+        }else{
+            break;
+        }
+        }
+        return 0;
+         
+    }
+
+
+
+
